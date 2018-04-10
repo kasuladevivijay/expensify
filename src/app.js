f95a5360e6e5039cc34fd69838f38843a38ca47d
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-10 11:23:25 +0530

    setting up routes and 404 using BrowserRouter, Route & Switch

--- src/app.js
+++ src/app.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const app = document.querySelector('#app');
+const ExpenseDashboard = () => (
+	<div>
+        This is Expense Dashboard page
+	</div>
+);
 
-ReactDOM.render(<p>App js running</p>, app);
+const AddExpense = () => (
+	<div>
+        This is AddExpense page
+	</div>
+);
+
+const EditExpense = () => (
+	<div>
+        This is EditExpense page
+	</div>
+);
+
+const Help = () => (
+	<div>
+        This is Help page
+	</div>
+);
+
+const NotFound = () => (
+	<div>
+        404!
+	</div>
+);
+
+const app = document.querySelector('#app');
+const routes = (
+	<Router>
+		<Switch>
+			<Route path='/' component={ExpenseDashboard} exact={true}/>
+			<Route path='/create' component={AddExpense} />
+			<Route path='/edit' component={EditExpense} />
+			<Route path='/help' component={Help} />
+			<Route component={NotFound}/>
+		</Switch>
+	</Router>
+);
+
+ReactDOM.render(routes, app);
