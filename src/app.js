31d9938567e2a1f735a934a45676b35f506176db
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-10 09:59:26 +0530

    project structure and setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const app = document.querySelector('#app');
+
+ReactDOM.render(<p>App js running</p>, app);
