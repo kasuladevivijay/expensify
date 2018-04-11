3f12843ce5819eb8e8b74a7fd5544341955aacc4
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-11 17:21:04 +0530

    redux actions with additional keys

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,29 +1,47 @@
-import { createStore } from 'redux';
+import {
+	createStore
+} from 'redux';
 
-const store = createStore((state = {count: 0}, action)=> {
+const store = createStore((state = {
+	count: 0
+}, action) => {
 	// redux actions definition
 	switch (action.type) {
 	case 'INCREMENT':
+		const incrementBy = typeof(action.incrementBy) === 'number' ? action.incrementBy : 1;
 		return {
-			count: state.count + 1
+			count: state.count + incrementBy
 		};
 	case 'DECREMENT':
+		const decrementBy = typeof(action.decrementBy) === 'number' ? action.decrementBy : 1;
 		return {
-			count: state.count - 1
-		};
+			count: state.count - decrementBy
+        };
+    case 'SET':
+        return {
+            count: action.count
+        };
 	case 'RESET':
 		return {
 			count: 0
-		};        
-	default: 
+		};
+	default:
 		return state;
 	}
+});
+
+// Redux Subscribe
+
+store.subscribe(() => {
+	console.log(store.getState());
 });
 
 // Calling Redux Actions
 
 store.dispatch({
-	type: 'INCREMENT'
+	// action dispact method with additional keys
+	type: 'INCREMENT',
+	incrementBy: 4
 });
 
 store.dispatch({
@@ -31,13 +49,20 @@ store.dispatch({
 });
 
 store.dispatch({
-	type: 'INCREMENT'
+	// action dispact method with additional keys
+	type: 'DECREMENT',
+	decrementBy: 3
 });
 
-console.log(store.getState());
+store.dispatch({
+	type: 'DECREMENT'
+});
 
 store.dispatch({
 	type: 'RESET'
 });
 
-console.log(store.getState());
+store.dispatch({
+	type: 'SET',
+	count: 100
+});
