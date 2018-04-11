c56fa2ee26dd324f818b5d419d212d275c4ffba1
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-11 16:12:24 +0530

    Redux actions

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,5 +1,31 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 0})=>{
-	return state;
+const store = createStore((state = {count: 0}, action)=> {
+	// redux actions definition
+	switch (action.type) {
+	case 'INCREMENT':
+		return {
+			count: state.count + 1
+		};
+	case 'DECREMENT':
+		return {
+			count: state.count - 1
+		};    
+	default: 
+		return state;
+	}
 });
+
+// Calling Redux Actions
+
+store.dispatch({
+	type: 'INCREMENT'
+});
+
+console.log(store.getState());
+
+store.dispatch({
+	type: 'DECREMENT'
+});
+
+console.log(store.getState());
