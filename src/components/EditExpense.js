568d1408edc203323ea210b7836474219988398a
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-11 09:48:31 +0530

    Router and props to display dynamic links in the url

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpense = () => (
-	<div>
-        This is EditExpense page
-	</div>
-);
+const EditExpense = (props) => {
+	console.log(props);
+	return (
+		<div>
+		Editing the page with expense id: {props.match.params.expId}
+		</div>
+	);
+};
 
 export default EditExpense;
