d5b77a83f8611904b33edd521072bde950153e48
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-20 00:03:42 +0530

    added hoc

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -5,6 +5,7 @@
  *  Prop Manipulation
  *  Abstract State
  */
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -40,3 +41,29 @@ const AuthInfo = requireAuthentication(Info);
 
 // ReactDOM.render(<AdminInfo isAdmin={true} info='These are the details'/>, document.querySelector('#app'));
 ReactDOM.render(<AuthInfo isAuthenticated={true} info='These are the details'/>, document.querySelector('#app'));
+*/
+
+console.log('combinantions');
+// factorial
+const factorial = (num) =>{
+	if(num === 0 || num === 1){
+		return 1;
+	}else if(num < 0){
+		return 'number should be positive';
+	}else {
+		return num * factorial(num-1);
+	}
+};
+
+const nCr = (n, r) => {
+	if( n > 0 && r > 0 && n >= r){
+		return(
+			(factorial(n)) / (factorial(r) * factorial(n-r))
+		);
+	}else {
+		return 'n and r should be positive integers';
+	}
+};
+
+console.log('nCr(7,7):', nCr(7,8));
+
